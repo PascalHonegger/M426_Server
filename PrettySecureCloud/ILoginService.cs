048591048ab95425fdc550d3c86e1a70f2242cff
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2016-09-13 11:41:21 +0200

    #17: Create intial solution

---
+++ PrettySecureCloud/ILoginService.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+
+namespace PrettySecureCloud
+{
+	[ServiceContract]
+	public interface ILoginService
+	{
+		[OperationContract]
+		bool MailValid(string mail);
+
+		[OperationContract]
+		void Register(string mail, string password);
+
+		[OperationContract]
+		void Login(string mail, string password);
+	}
+}
---
+++ PrettySecureCloud/LoginService.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrettySecureCloud
+{
+	public class LoginService : ILoginService
+	{
+		public bool MailValid(string mail)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Register(string mail, string password)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Login(string mail, string password)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
