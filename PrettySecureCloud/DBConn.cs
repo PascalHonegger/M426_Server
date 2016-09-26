38e74efe55fd0d945cb5d8240723d1b45aa7e42c
StarLordTheCoder <serafin717@gmail.com>
2016-09-26 21:00:32 +0200

    #6 DB Connection

---
+++ PrettySecureCloud/DBConn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Configuration;
+using System.Web;
+using PrettySecureCloud.Properties;
+
+namespace PrettySecureCloud
+{
+	public static class DBConn
+{
+		private static SqlConnection _conn;
+
+		private static SqlConnection Connection => _conn ?? (_conn = new SqlConnection(Settings.Default.ConnectionString) );
+
+		public static SqlCommand Command => Connection.CreateCommand();
+
+}
+}
